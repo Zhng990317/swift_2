584a953a881d9be6ec8d38db5e7ba1bcaf978418
Zhng990317 <zbq990317@yeah.net>
2018-09-29 08:58:08 +0800

    Initial Commit

---
+++ swift_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift_2
+//
+//  Created by s20171106333 on 2018/9/29.
+//  Copyright © 2018年 s20171106333. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
